9e902a84e0ced0a2461d5d73c3a1d0bdcbfb34d5
idallas93 <Dallas.Isaac1@gmail.com>
2024-04-17 08:46:27 -0600

    fixed capitain petzel

--- capitainPetzel.go
+++ capitainPetzel.go
@@ -2,27 +2,35 @@ package main
 
 import (
 	"log"
+	"strconv"
 	"github.com/gocolly/colly"
 )
 
 func updateCapitainPetzel(url string) {
 	c := colly.NewCollector()
 	var capitainPetzelExhibitions []Exhibition
-	c.OnHTML("exhibitions-grid-forthcoming_featured", func(e *colly.HTMLElement) {
+	c.OnHTML("#main_content", func(e *colly.HTMLElement) {
 		e.ForEach("a", func(i int, colElement *colly.HTMLElement) {
 			artistText := colElement.ChildText("h2")
 			title := colElement.ChildText("span.subtitle")
 			date := colElement.ChildText("span.date")
+			var year string = ""
+			if(len(date) > 4){
+				year = date[len(date)-4:]
+			}
 			location := "Karl-Marx-Allee 45, 10178 Berlin, Germany"
-			capitainPetzelExhibitions = append(capitainPetzelExhibitions, Exhibition{
-				Gallery:   "Capitain Petzel",
-				Location:  location,
-				Artist:    artistText,
-				Title:     title,
-				StartDate: date,
-				EndDate:   date,
-				Notes:     "",
-			})
+			num, _ := strconv.Atoi(year)
+			if year != "" && num >= 2024 {
+				capitainPetzelExhibitions = append(capitainPetzelExhibitions, Exhibition{
+					Gallery:   "Capitain Petzel",
+					Location:  location,
+					Artist:    artistText,
+					Title:     title,
+					StartDate: date,
+					EndDate:   date,
+					Notes:     "",
+				})
+			}
 		})
 	})
 	err := c.Visit(url)
